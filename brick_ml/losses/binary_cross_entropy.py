cb6e07077afb7b97861e1a41675a2a9bb7201a37
InvestedBrick <julianbrecht25@gmail.com>
2024-06-19 15:22:07 +0200

    Stabilized to avoid dividing by 0

--- brick_ml/losses/binary_cross_entropy.py
+++ brick_ml/losses/binary_cross_entropy.py
@@ -4,6 +4,10 @@ class binary_cross_entropy:
     def __init__(self) -> None:
         pass
     def loss(self,y_true,y_pred):
+        epsilon = 1e-10
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
         return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
     def loss_derivative(self,y_true,y_pred):
-        return((1 - y_true) / (1 - y_pred) - y_true / y_pred) / np.size(y_true)
+        epsilon = 1e-10
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return ((1 - y_true) / (1 - y_pred) - y_true / y_pred) / np.size(y_true)
