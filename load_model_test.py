15b4195f2a1f43038781e6e5436f2f3c2cbc03e4
InvestedBrick <julianbrecht25@gmail.com>
2024-06-21 18:29:32 +0200

    Mnist test

--- load_model_test.py
+++ load_model_test.py
@@ -1,15 +1,22 @@
 import brick_ml.models.Sequential as Sequential
-import sklearn.datasets
 import brick_ml.utility as util
+import numpy as np
+import keras.datasets  as keras_data
 
-data_set = sklearn.datasets.load_iris()
-X = data_set["data"]
-y = [util.vectorize(size=3,idx=i) for i in data_set["target"]]
-names = data_set["target_names"]
-X_train,X_test,y_train,y_test = util.test_train_split(X,y,split_size=0.7)
+(X_train, y_train), (X_test, y_test) = keras_data.mnist.load_data()
+
+X_train = X_train.astype("float32") / 255
+X_test = X_test.astype("float32") / 255 
+X_train = X_train.reshape(len(X_train), 28 * 28,)
+X_test = X_test.reshape(len(X_test),1, 28 * 28,) # reshape for a btach size of 1 for evaluation
+y_train = np.array([util.vectorize(size=10,idx=i) for i in y_train])
+y_test = np.array([util.vectorize(size=10,idx=i) for i in y_test])
+
+y_train = y_train.reshape(len(y_train),10)
+y_test = y_test.reshape(len(y_test),1,10)
 
 model = Sequential.Sequential(None,None)
-model.load("iris")
+model.load("mnist_dense")
 
 accuracy = model.evaluate(X_test,y_test)
 print(f"Accuracy: {accuracy*100:.2f} %")
